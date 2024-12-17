9d3b34eefdfea0d97ac208cdee5056df876370c0
nunori <nunori@users.noreply.github.com>
2024-12-17 05:31:32 +0000

    deploy: 11c67772dc19a891ca7c74ffb563d7821ad8de86

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1734413491',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
